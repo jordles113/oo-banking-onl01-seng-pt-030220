12c3aea855075dfd73607798db706cae2387e5fb
Jordan Romero <jordan.t.romero@gmail.com>
2020-03-30 21:38:07 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -12,5 +12,23 @@ class Transfer
   end 
   
   def execute_transaction
-  end 
+  	if self.valid? && self.sender.balance > self.amount && self.status == "pending"
+	  	self.sender.balance -= self.amount
+	  	self.receiver.balance += self.amount
+	  	self.status = "complete"
+	  else  	
+	  	self.status = "rejected"
+	  	"Transaction rejected. Please check your account balance."
+	  end
+  end
+
+  def reverse_transfer
+  	if self.status == "complete"
+  		self.sender.balance = self.sender.balance += self.amount
+  		self.receiver.balance = self.receiver.balance -= self.amount
+  		self.status = "reversed"
+  	else
+
+  	end
+  end
 end
