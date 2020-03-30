63140b6dfd732860f2a961d1cd7994dbc3adebb6
Jordan Romero <jordan.t.romero@gmail.com>
2020-03-30 21:24:54 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,9 +1,13 @@
 class BankAccount
-  attr_accessor :name, :balance, :status 
+  attr_accessor :balance, :status 
+  attr_reader :name 
   def initialize(name)
     @name = name 
     @balance = 1000
     @status = "open"
+  end 
+  
+  def deposit 
   end 
 
 end
