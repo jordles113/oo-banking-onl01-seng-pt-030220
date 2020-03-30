32ae80d297dea813cfde203df79f0e8021874c0c
Jordan Romero <jordan.t.romero@gmail.com>
2020-03-30 21:36:06 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,9 +1,9 @@
 class Transfer
   attr_accessor :sender, :receiver, :amount, :status
-  def initialize(sender, receiver, transfer_amount)
+  def initialize(sender, receiver, amount)
     @sender = sender
     @receiver = receiver
     @status = "pending"
-    transfer_amount = 100
+    @amount = 50
   end 
 end
